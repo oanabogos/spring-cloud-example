b317a8bb0af9ed342ec680b1df8920b45c39ce49
Oana Bogos <catalina.bogos@softvision.ro>
2017-01-06 11:53:20 +0200

    Enable monitor and integrate amqp between server and clients

--- product-catalog-service/src/main/java/ro/oanabogos/spring/services/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/ro/oanabogos/spring/services/ProductCatalogServiceApplication.java
@@ -9,4 +9,5 @@ public class ProductCatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductCatalogServiceApplication.class, args);
     }
+
 }
---
+++ shopping-cart-service/src/main/java/ro/oanabogos/spring/services/controller/ShoppingCartController.java
@@ -0,0 +1,34 @@
+package ro.oanabogos.spring.services.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ShoppingCartController {
+
+    @Value("${message}")
+    private String message;
+
+    @Value("${location}")
+    private String location;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @RequestMapping("/message")
+    public String getMessage() {
+        return this.message + " from " + this.location;
+    }
+
+    @RequestMapping("/dataSource")
+    public String getDatasourceCredentials() {
+        return "username:" + this.username + " password: " + this.password;
+    }
+
+}
